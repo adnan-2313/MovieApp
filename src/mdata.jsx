95cd01fcef79f91f18eda335dca021f4ae9f9f9f
Adnan <ak567845@gmail.com>
2024-01-21 23:47:10 +0530

    updating code

---
+++ src/mdata.jsx
@@ -0,0 +1,52 @@
+const mdata = [{
+    src:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5a8a6131-48da-4feb-9bfe-3a555b3312c2/deez3m0-5acff1a5-bcf5-4995-885e-570dfa9cdc1d.jpg/v1/fill/w_734,h_1088,q_70,strp/captain_america__the_winter_soldier_by_darksideofdesign_deez3m0-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTMzNCIsInBhdGgiOiJcL2ZcLzVhOGE2MTMxLTQ4ZGEtNGZlYi05YmZlLTNhNTU1YjMzMTJjMlwvZGVlejNtMC01YWNmZjFhNS1iY2Y1LTQ5OTUtODg1ZS01NzBkZmE5Y2RjMWQuanBnIiwid2lkdGgiOiI8PTkwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.DS1ltmgbBzDwWF9Cafq3b_h0u9aCTaNdsqv9CVS9qjs",
+    title:"Marvel Cinematic Universe",
+     sname:"Captain America:Winter Soilder" ,
+     href:"https://www.hotstar.com/in/movies/captain-america-the-winter-soldier/1260016410" 
+},
+{
+    src:"https://images-cdn.ubuy.co.in/65809c8a3976f525d26ef0ea-movie-poster-iron-man-3-2-sided-rare.jpg" ,
+    href:"https://www.hotstar.com/in/movies/iron-man-3/1660000042?filters=content_type%3Dmovie",
+    title:"Marvel Cinematic Universe",
+    sname:"Iron Man 3"  
+},
+{
+    src:"https://c8.alamy.com/comp/PM83GB/marvels-the-avengers-2012-poster-PM83GB.jpg",
+     href:"https://www.hotstar.com/in/movies/marvels-the-avengers/1660000015?filters=content_type%3Dmovie",
+     title:"Marvel Cinematic Universe",
+     sname:"The Avengers"
+},
+{
+    src:"https://m.media-amazon.com/images/I/81mf3QQFA2L._AC_UF1000,1000_QL80_.jpg",
+    title:"Marvel Cinematic Universe",
+    sname:"Avengers Age Of Ultron",
+    href:"https://www.hotstar.com/in/movies/marvels-avengers-age-of-ultron/1260018315?filters=content_type%3Dmovie"
+  
+},
+{
+    sname: 'Black Panther',
+    src: 'https://i.etsystatic.com/18242346/r/il/efaf30/4386825669/il_570xN.4386825669_ry4z.jpg',
+    href: 'https://www.hotstar.com/blackpanther',
+    title:"Marvel Cinematic Universe",
+  },
+  {
+    sname: 'Spider-Man: Homecoming',
+    src: 'https://media.vanityfair.com/photos/592592596736887ada186bcd/master/w_1600%2Cc_limit/spider-man-homecoming-SMH_DOM_Online_FNL_1SHT_3DRD3DIMX_AOJ_300dpi_01_rgb.jpg',
+   href: 'https://www.hotstar.com/spiderman',
+    title:"Marvel Cinematic Universe",
+  },
+  {
+    sname: 'Thor: Ragnarok',
+    src: 'https://i.ebayimg.com/images/g/BRAAAOSw-Lxi~No1/s-l1200.webp',
+   href: 'https://www.hotstar.com/thor',
+    title:"Marvel Cinematic Universe",
+  },
+  {
+    sname: 'Doctor Strange',
+    src: 'https://m.media-amazon.com/images/I/81zCY9g4WOL._AC_UF894,1000_QL80_.jpg',
+   href: 'https://www.hotstar.com/doctorstrange',
+    title:"Marvel Cinematic Universe",
+  },
+]
+
+export default mdata;
